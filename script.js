ff586669cf471101aca17686b5acac4fa79e3711
Alexandra Mészáros <alexandrameszaros@kalina.kea.local>
2020-08-28 11:55:10 +0200

    start

---
+++ script.js
@@ -0,0 +1,28 @@
+//fetch data
+
+fetch("https://kea-alt-del.dk/t5/api/productlist")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+
+
+
+
+        }
+
+    )
+
+console.log(data)
+
+
+//loop through products
+
+
+//finding temaplte
+
+
+//fill out template
+
+
+//append
