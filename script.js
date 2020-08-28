93efd5a07135cdf0c59fa5e8ab90afaa2de13fb1
Alexandra Mészáros <alexandrameszaros@kalina.kea.local>
2020-08-28 12:45:02 +0200

    corrected HTML structure

--- script.js
+++ script.js
@@ -5,24 +5,26 @@ fetch("https://kea-alt-del.dk/t5/api/productlist")
         return response.json();
     })
     .then(function (data) {
-
-
-
-
-        }
-
-    )
-
-console.log(data)
-
+        console.log(data)
+        dataReceived(data);
+    })
 
 //loop through products
+function dataReceived(products) {
+    products.forEach(showProduct)
+}
 
+//executed once for each product
+function showProduct(myProduct) {
 
-//finding temaplte
+    //finding template
+    const temp = document.querySelector("#productTemplate").content;
+    //clone the template
+    const myCopy = temp.cloneNode(true);
+    //fill out template
+    myCopy.querySelector(".dishname").textContent = myProduct.name;
+    //append
+    const parentElem = document.querySelector("section#starters");
+    parentElem.appendChild(myCopy)
+}
 
-
-//fill out template
-
-
-//append
