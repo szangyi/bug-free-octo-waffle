41852e94b0bd371c0feafb87f231150f4b197cbd
Alexandra Mészáros <alexandrameszaros@Alexandras-MacBook-Pro-2.local>
2020-09-01 10:47:49 +0200

    vegetarian and soldout classes added to javascript

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ fetch("https://kea-alt-del.dk/t5/api/productlist")
 //loop through products
 function dataReceived(products) {
     products.forEach(showProduct);
-//    products.forEach(showInfo)
+    //    products.forEach(showInfo)
 }
 
 //executed once for each product
@@ -28,7 +28,7 @@ function showProduct(myProduct) {
     myCopy.querySelector(".data_discount").textContent = `-${myProduct.discount}%`;
     myCopy.querySelector(".data_price").textContent = `${myProduct.price}kr`;
     //append
-    const parentElem = document.querySelector("section#starters");
+    const parentElem = document.querySelector("section#starter");
 
 
     if (!myProduct.discount) {
@@ -36,6 +36,35 @@ function showProduct(myProduct) {
         console.log("nodiscount");
         myCopy.querySelector(".data_discount").classList.toggle("hidden")
     }
+    if (myProduct.vegetarian) {
+        myCopy.querySelector(".infoveg").classList.remove("hidden")
+    };
+
+    if (myProduct.soldout) {
+        myCopy.querySelector(".product").classList.add("soldout")
+    }
+
+//setup classes for filtering
+//1.find article
+const article = myCopy.querySelector("article")
+
+//2.add classes
+if (myProduct.vegetarian) {
+    article.classList.add("vegetarian")
+}
+
+const veggiefilter = document.querySelector("#veggiefilter");
+veggiefilter.addEventListener("click", veggieFilterClicked);
+
+
+function veggieFilterClicked() {
+    //select all non veggie
+    const articles = document.querySelectorAll("article:not(.vegetarian)");
+    console.log(articles);
+    veggiefilter.classList.toggle("active");
+}
+
+
 
 
 
@@ -44,37 +73,50 @@ function showProduct(myProduct) {
 }
 
 
-//function showInfo(myProduct) {
-//    const temp = document.querySelector("#productTemplate").content;
-//    const myCopy = temp.cloneNode(true);
+function init() {
+    fetch("https://kea-alt-del.dk/t5/api/categories").then(r => r.json()).then(function (data) {
+        categoriesReceived(data)
+    })
+}
+
+init();
+
+function categoriesReceived(cats) {
+    //    createNavigation(cats);
+    createSections(cats);
+}
+
+
+
+function createSections(categories) {
+    categories.forEach(category => {
+        const section = document.createElement("section");
+        section.setAttribute("id", category);
+        const h1 = document.createElement("h1");
+        h1.textContent = category;
+        section.appendChild(h1);
+        document.querySelector(".productlist").appendChild(section);
+    })
+}
+
+//function createNavigation(categories) {
+//    categories.forEach(cat => {
 //
 //
-//    //setup classes for filtering
-//    //1.find article
-//    const article = myCopy.querySelector("article")
+//        const a = document.createElement("a");
 //
-//    //2.add classes
-//    if (myProduct.vegetarian) {
-//        article.classList.add("vegetarian")
-//    }
+//        text.content = cat;
+//        a.setAttribute("href", `${cat}`);
 //
-//    const veggiefilter = document.querySelector("#veggiefilter");
-//    veggiefilter.addEventListener("click", veggieFilterClicked);
-//
-//
-//    function veggieFilterClicked() {
-//        //select all non veggie
-//        const articles = document.querySelectorAll("article:not(.vegetarian)");
-//        console.log(articles);
-//        veggiefilter.classList.toggle("active");
-//    }
-//
-//
-//    //    if (myProduct.vegetarian) {
-//    //        myCopy.querySelector(".infoveg").classList.remove("hidden")
-//    //    };
-//
-//    //    if (myProduct.soldout) {
-//    //        myCopy.querySelector(".product").classList.add("soldout")
-//    //    }
+//        document.querySelector("#nav").appendChild(a);
+//    })
 //}
+
+
+
+
+
+
+
+
+
